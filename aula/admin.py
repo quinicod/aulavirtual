ae10831125deaa0ee5522544ccd0fcca650e3d92
quino <joaquinguzmangarciaplata@gmail.com>
2019-02-19 16:51:00 +0100

    modeladmin

--- aula/admin.py
+++ aula/admin.py
@@ -7,13 +7,18 @@ from .models import User_asignatura
 from .models import Asignatura
 from .models import Evento, File_Evento
 
+class Fileseccion(admin.ModelAdmin):
+    list_display = ('file', 'descripcion', 'id_seccion')
+    fields = ['file', 'descripcion', ('id_seccion')]
+    
+
 admin.site.register(Perfil)
 admin.site.register(Nivel)
 admin.site.register(Curso)
 admin.site.register(User_asignatura)
 admin.site.register(Asignatura)
 admin.site.register(Seccion)
-admin.site.register(File_seccion)
+admin.site.register(File_seccion, Fileseccion)
 admin.site.register(Evento)
 admin.site.register(File_Evento)
 
